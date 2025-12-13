766aeed9e8ca6048af48017989582b2be062f9db
ascendho <coderhs@foxmail.com>
2025-12-13 10:43:02 +0800

    上传地址空间代码

---
+++ code/vm-intro/va.c
@@ -0,0 +1,13 @@
+// To compile: make
+// To run: ./va
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("location of code : %p\n", main);
+    printf("location of heap : %p\n", malloc(100e6));
+    int x = 3;
+    printf("location of stack: %p\n", &x);
+    return 0;
+}
